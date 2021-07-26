cdeb454da4687f350edc79a93364ce8d687d99c3
wegiveyoubrand <wegiveyoubrand@gmail.com>
2021-07-26 08:14:06 +0100

    Updated

--- src/js/Modules/_accordion.js
+++ src/js/Modules/_accordion.js
@@ -1,14 +1,15 @@
-const accordion = document.querySelectorAll("faq-card");
+const accordions = document.querySelectorAll("faq-card");
 
+accordions.forEach((accordion) => {
+  accordion.addEventListener("click", () => {
+    accordion.classList.toggle("active");
+  });
+});
+
+/* Deprecated Version of the above code 
 for (let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener("click", function () {
     this.classList.toggle("active");
   });
 }
-// accordions.forEach((accordion) => {
-//   accordion.addEventListener("click", () => {
-//    accordion.classList.toggle('active')
-//   });
-// });
-
-// console.log(accordions);
+*/
